73a5312fa74243fba9cfcbae8e14f1e00e733e11
Edson lopez <edson_lopez@hotmail.com.ar>
2019-01-02 22:23:54 -0400

    servicio clientes observable

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
-
+import { Cliente } from './cliente'
+import { Observable } from 'rxjs';
+import { of } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,5 +10,7 @@ export class ClienteService {
 
   constructor() { }
 
-
+  getClientes(): Observable<Cliente[]>{
+    return of(CLIENTES);
+  }
 }
