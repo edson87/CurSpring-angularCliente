4eda9cf820c57c0dc225e7672953d29452be0ba9
Edson <edson_lopez@hotmail.com.ar>
2019-01-25 15:56:06 -0400

    Paginador aumento de inicio fin y modificacion del tamaño de paginas que llegan

--- src/app/paginator/paginator.component.ts
+++ src/app/paginator/paginator.component.ts
@@ -1,24 +1,46 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, OnChanges, Input} from '@angular/core';
 
 @Component({
   selector: 'paginator-nav',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.css']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
   @Input() paginador: any;
   paginas: number[];
   inicio: number;
   fin: number;
+  desde: number;
+  hasta: number;
 
-  constructor( )  { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.paginas = new Array(this.paginador.totalPages).fill(0).map((valor, indice) => indice + 1);
+    /*this.paginas = new Array(this.paginador.totalPages).fill(0).map((valor, indice) => indice + 1);
     console.log(this.paginador);
     let tamaño = this.paginas.length;
     this.inicio=0;
-    this.fin = tamaño - 1;
+    this.fin = tamaño - 1;*/
   }
 
+  ngOnChanges() {
+    this.desde = Math.min(Math.max(1, this.paginador.number - 4), this.paginador.totalPages - 5);
+    this.hasta = Math.max(Math.min(this.paginador.totalPages, this.paginador.number + 4), 6);
+
+    if (this.paginador.totalPages > 5) {
+      this.paginas = new Array(this.hasta - this.desde + 1).fill(0).map((valor, indice) => indice + this.desde);
+      console.log(this.paginador);
+      let tamaño = this.paginas.length;
+      this.inicio = 0;
+      this.fin = tamaño - 1;
+    } else {
+
+      this.paginas = new Array(this.paginador.totalPages).fill(0).map((valor, indice) => indice + 1);
+      console.log(this.paginador);
+      let tamaño = this.paginas.length;
+      this.inicio = 0;
+      this.fin = tamaño - 1;
+    }
+  }
 }
